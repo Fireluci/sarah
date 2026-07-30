f2534fd13870df1840c7c4cb93f21e9a0761f16e
Fireluci <120563797+Fireluci@users.noreply.github.com>
2026-07-30 08:27:22 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -5,6 +5,7 @@ dbclient = pymongo.MongoClient(DB_URI)
 database = dbclient[DB_NAME]
 
 user_data = database['users']
+config_data = database['config']
 
 async def present_user(user_id : int):
     found = user_data.find_one({'_id': user_id})
@@ -26,16 +27,16 @@ async def del_user(user_id: int):
     user_data.delete_one({'_id': user_id})
     return
 
-async def update_shortener(user_id: int, site: str, api: str):
-    user_data.update_one(
-        {'_id': user_id},
+async def update_shortener(site: str, api: str):
+    config_data.update_one(
+        {'_id': 'shortener_config'},
         {'$set': {'shortener': site, 'shortener_api': api}},
         upsert=True
     )
     return
 
-async def get_shortener(user_id: int):
-    user = user_data.find_one({'_id': user_id})
-    if user and 'shortener' in user and 'shortener_api' in user:
-        return user['shortener'], user['shortener_api']
+async def get_shortener():
+    config = config_data.find_one({'_id': 'shortener_config'})
+    if config and 'shortener' in config and 'shortener_api' in config:
+        return config['shortener'], config['shortener_api']
     return None, None
