8d924fa44708bee2d8439c5ce200cbf38548e4a6
Fireluci <120563797+Fireluci@users.noreply.github.com>
2026-08-21 13:53:39 +0530

    Refactor bot.py for improved readability
    Refactor import statements and improve code formatting for better readability. Adjust indentation and line breaks for consistency.

--- bot.py
+++ bot.py
@@ -7,7 +7,18 @@ from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, FORCE_SUB_LINK, CHANNEL_ID, PORT
+from config import (
+    API_HASH,
+    APP_ID,
+    LOGGER,
+    TG_BOT_TOKEN,
+    TG_BOT_WORKERS,
+    FORCE_SUB_CHANNEL,
+    FORCE_SUB_LINK,
+    CHANNEL_ID,
+    PORT
+)
+
 
 class Bot(Client):
     def __init__(self):
@@ -28,27 +39,43 @@ class Bot(Client):
         usr_bot_me = await self.get_me()
         self.uptime = datetime.now()
 
-if FORCE_SUB_CHANNEL:
-    self.invitelink = FORCE_SUB_LINK
+        if FORCE_SUB_CHANNEL:
+            self.invitelink = FORCE_SUB_LINK
+
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+
+            test = await self.send_message(
+                chat_id=db_channel.id,
+                text="Test Message"
+            )
             await test.delete()
+
         except Exception as e:
             self.LOGGER(__name__).warning(e)
-            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
+            self.LOGGER(__name__).warning(
+                f"Make Sure bot is Admin in DB Channel, "
+                f"and Double check the CHANNEL_ID Value, "
+                f"Current Value {CHANNEL_ID}"
+            )
             self.LOGGER(__name__).info("\nBot Stopped")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info("Bot Started Successfully.")
         self.username = usr_bot_me.username
-        #web-response
+
+        # web-response
         app = web.AppRunner(await web_server())
         await app.setup()
+
         bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
+        await web.TCPSite(
+            app,
+            bind_address,
+            PORT
+        ).start()
 
     async def stop(self, *args):
         await super().stop()
